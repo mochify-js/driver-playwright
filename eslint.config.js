9f57797535552e46198776705ddae6b8fe8f4f99
Yashar Fakhari <6448697+YasharF@users.noreply.github.com>
2025-08-16 12:26:57 -0700

    chore: update dependencies

---
+++ eslint.config.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const studio_eslint_config = require('@studio/eslint-config').default;
+
+module.exports = [
+  ...studio_eslint_config,
+  {
+    files: ['test/**/*.js', '**/*.test.js', '**/*.integration.js'],
+    languageOptions: {
+      globals: {
+        describe: 'readonly',
+        it: 'readonly',
+        before: 'readonly',
+        after: 'readonly',
+        beforeEach: 'readonly',
+        afterEach: 'readonly'
+      }
+    }
+  }
+];
